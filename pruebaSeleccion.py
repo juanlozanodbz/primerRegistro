6dc741a9f86d2f9ffd1be99146debe8186843ed0
Juan S. Lozano <A01374452@itesm.mx>
2018-09-04 09:47:15 -0500

    l

---
+++ pruebaSeleccion.py
@@ -0,0 +1,12 @@
+# Autor: Juan Sebastian Lozano Derbez
+# Prueba seleccion simple/doble
+
+def main():
+    carrera = input("Carrera: ")
+
+    if carrera.upper() == "IMT":
+        print("Estas en el salon equivocado")
+    else:
+        print ("Estas en el salon correcto")
+
+main()
